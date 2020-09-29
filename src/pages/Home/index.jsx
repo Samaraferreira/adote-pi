5422de2604dc292c3ec15b4a694c31ce648f7b77
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-09-28 23:56:55 -0300

    adaptando página Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,6 +11,7 @@ import dogImg from '../../assets/img-dog.svg';
 import birdImg from '../../assets/img-bird.svg';
 import catImg from '../../assets/img-cat.svg';
 import hamsterImg from '../../assets/img-hamster.svg';
+import adoptImg from '../../assets/adopt.svg';
 
 function Home() {
   return (
@@ -19,39 +20,40 @@ function Home() {
       <Divider />
       <main className="container">
         <BoxAdoptUs />
-        <section class="categorias">
-          <h2>Categorias</h2>
-          <div class="cards">
-              <div class="item_cards dog">
+        <section className="categorias">
+          <h2 className="title">Categorias</h2>
+          <div className="cards">
+              <div className="item_cards dog">
                   <img src={dogImg} alt="Cachorro" />
-                  <h3 class="descricao">Cachorros</h3>
+                  <h3 className="descricao">Cachorros</h3>
               </div>
-              <div class="item_cards cat">
+              <div className="item_cards cat">
                   <img src={catImg} alt="Gato" />
-                <h3 class="descricao">Gatos</h3>
+                <h3 className="descricao">Gatos</h3>
               </div>
-              <div class="item_cards bird">
+              <div className="item_cards bird">
                   <img src={birdImg} alt="Passaros" />
-                  <h3 class="descricao">Pássaros</h3>
+                  <h3 className="descricao">Pássaros</h3>
               </div>
-              <div class="item_cards other">
+              <div className="item_cards other">
                   <img src={hamsterImg} alt="Outros" />
-                  <h3 class="descricao">Outros</h3>
+                  <h3 className="descricao">Outros</h3>
               </div>
           </div>
         </section>
-
-        <section class="informacoes">
-          <h2>Por que adotar?</h2>
-          <div class="infocomponentes">
-              <p class="primeiro-texto">
-                  Adotar é sempre um gesto de muito amor e carinho pois, além de proporcionar um novo lar para o pet,
-                  você ganhará um novo amigo que será sempre grato e um fiel companheiro.
+        <section className="infos">
+          <h2 className="title">Por que adotar?</h2>
+          <div className="infos-content">
+            <div>
+              <p className="primeiro-texto">
+                Adotar é sempre um gesto de muito amor e carinho pois, além de proporcionar um novo lar para o pet,
+                você ganhará um novo amigo que será sempre grato e um fiel companheiro.
               </p>
-              <p class="segundo-texto">
-                  Adotar é um ato consciente e abandonar é crime. Não adote por impulso, adote por amor!
+              <p className="segundo-texto">
+                Adotar é um ato consciente e abandonar é crime. Não adote por impulso, adote por amor!
               </p>
-              <img src="img/adopt.svg" alt="" />
+            </div>
+            <img src={adoptImg} alt="Adote" />
           </div>
         </section>
       </main>
