f072ca84c436a60972e8384841f33c3322477279
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-10-04 12:54:24 -0300

    Add CreatePet function

--- src/services/api.js
+++ src/services/api.js
@@ -9,3 +9,21 @@ export const getPetsByCategory = async (category) => {
     console.log(err)
   }
 }
+
+export const createPet = async (pet) => {
+  try {
+    const response = await fetch(`${baseURL}/pets`, {
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      method: 'post',
+      body: JSON.stringify(pet)
+    });
+
+    console.log(response.json())
+    return response.json();
+  } catch (err) {
+    console.log(err)
+  }
+}
