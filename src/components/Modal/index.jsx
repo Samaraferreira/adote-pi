deeaef506634bcac7744b6c286525ba97736ad5d
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-10-04 12:44:34 -0300

    Create input for image

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,43 @@
-import React, { useContext }  from 'react';
+import React, { useState, useContext, useMemo }  from 'react';
+
+import { createPet } from '../../services/api';
 
 import { ModalContext } from '../../App';
 
+import avatarImage from '../../assets/thumb.svg';
 import closeIcon from '../../assets/icons/icon-close.svg';
 
 import './styles.css';
 
 function Modal() {
   const { openModal, setOpenModal } = useContext(ModalContext);
+  const [avatar, setAvatar] = useState(null);
+  const [name, setName] = useState('Chico');
+  const [whatsapp, setWhatsapp] = useState('5582945654543');
+  const [type, setType] = useState('dog');
+  const [age, setAge] = useState('6 meses');
+  const [sex, setSex] = useState('macho');
+
+  const preview = useMemo(() => {
+    return avatar ? URL.createObjectURL(avatar) : avatarImage;
+  }, [avatar]);
+
+  async function handleNewPet(event) {
+    event.preventDefault();
+
+    if(avatar && name && whatsapp && type && age && sex) {
+      const data = new FormData();
+
+      data.append('avatar', avatar);
+      data.append('name', name);
+      data.append('whatsapp', whatsapp);
+      data.append('type', type);
+      data.append('age', age);
+      data.append('sex', sex);
+
+      await createPet(data);
+    }
+  }
 
   return (
     <section className={`modal-container ${openModal && 'modal-active' } `}>
@@ -17,6 +47,29 @@ function Modal() {
           <img src={closeIcon} alt="fechar"/>
         </button>
       </header>
+      <main>
+        <form onSubmit={handleNewPet}>
+          <div id="input-file">
+            <span
+              className="avatar-image"
+              style={{ backgroundImage: `url(${preview})` }}
+            />
+            <input
+              id="file"
+              name="avatar"
+              placeholder="Adicionar avatar"
+              type="file"
+              accept=".png, .jpeg, .jpg"
+              onChange={event => setAvatar(event.target.files[0])}
+            />
+            <label className="upload-label" htmlFor="file">
+              Adicionar avatar
+            </label>
+          </div>
+
+          <button type="submit">Adicionar</button>
+        </form>
+      </main>
     </section>
   );
 }
